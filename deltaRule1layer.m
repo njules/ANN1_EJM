94e53771ec0d40f53e291f91aac57682e28349f7
mmaatthieuu <mpdsc@kth.se>
2018-01-26 13:49:12 +0100

    Add perceptron (but with a weird little offset) Yes cause before it was the delta-rule and not the perceptron

--- deltaRule1layer.m
+++ deltaRule1layer.m
@@ -1,17 +1,16 @@
-function [W] = perceptron1Layer(patterns,targets,epochs,eta)
-%PERCEPTRON Do crazy shit man
+function [W] = deltaRule1layer(patterns,targets,epochs,eta)
+%deltaRule1layer Do crazy shit man
 %   function [W] = perceptron1Layer(patterns,targets,epochs,eta)
     rng(1);
     
     nData=size(patterns,2);
-    patterns=[patterns; ones(1,nData)];
+    patterns=[patterns; ones(1,nData)]
     
-    W=randn(size(targets,1), size(patterns,1));
-%     bla=W*patterns
+    W=randn(size(targets,1), size(patterns,1))
+    bla=W*patterns
     for ii=1:epochs
-%         (W*patterns-targets)*(W*patterns-targets)'
-%         Delta_W = -eta*(W*patterns-targets)*patterns';
-        Delta_W=-eta*(sign(W*patterns)-targets)*patterns'./2;
+        (W*patterns-targets)*(W*patterns-targets)'
+        Delta_W = -eta*(W*patterns-targets)*patterns';
         W=W+Delta_W;
 
 %         delta_w=zeros(size(targets,1));
@@ -24,6 +23,5 @@ function [W] = perceptron1Layer(patterns,targets,epochs,eta)
 %         W=W+delta_w;
 %     
     end
-
 end
 
