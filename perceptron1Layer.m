5effc9291a06348c8f5b2e021989f569158f693b
Elisabeth D <elisabeth.degrand@hotmail.fr>
2018-01-26 15:06:13 +0100

    :q

--- perceptron1Layer.m
+++ perceptron1Layer.m
@@ -4,25 +4,17 @@ function [W] = perceptron1Layer(patterns,targets,epochs,eta)
     rng(1);
     
     nData=size(patterns,2);
-    patterns=[patterns; ones(1,nData)]
+    patterns=[patterns; ones(1,nData)];
     
-    W=randn(size(targets,1), size(patterns,1))
-    bla=W*patterns
+    W=randn(size(targets,1), size(patterns,1));
+    error=zeros(epochs,1);
     for ii=1:epochs
         (W*patterns-targets)*(W*patterns-targets)';
         Delta_W = -eta*(W*patterns-targets)*patterns';
         W=W+Delta_W;
-
-%         delta_w=zeros(size(targets,1));
-%         for jj=1:size(targets,1)
-%             for kk=1:nData
-%                 delta_w(jj)=delta_w(jj)-eta*patterns(jj,kk)*(W*patterns(:,kk)-targets(jj));
-%                 
-%             end
-%         end
-%         W=W+delta_w;
-%     
+        %error(ii)= sum(sum(abs(sign(W*patterns) - targets)./2));
+        error(ii)= sum(abs(sign(W*patterns) - targets));
     end
-
+%     plot(error)
 end
 
