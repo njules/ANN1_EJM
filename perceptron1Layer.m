730d9a058fb439c67c9552f73a554aa8635c4abb
Elisabeth D <elisabeth.degrand@hotmail.fr>
2018-01-26 15:23:38 +0100

    zfz

---
+++ perceptron1Layer.m
@@ -0,0 +1,21 @@
+function [W] = perceptron1Layer(patterns,targets,epochs,eta)
+%PERCEPTRON Do crazy shit man
+%   function [W] = perceptron1Layer(patterns,targets,epochs,eta)
+    rng(1);
+    
+    nData=size(patterns,2);
+    patterns=[patterns; ones(1,nData)];
+    
+    W=randn(size(targets,1), size(patterns,1));
+    error=zeros(epochs,1);
+    for ii=1:epochs
+%         (W*patterns-targets)*(W*patterns-targets)'
+%         Delta_W = -eta*(W*patterns-targets)*patterns';
+        Delta_W=-eta*(sign(W*patterns)-targets)*patterns'./2;
+        W=W+Delta_W;
+        %error(ii)= sum(sum(abs(sign(W*patterns) - targets)./2));
+        error(ii)= sum(abs(sign(W*patterns) - targets));
+    end
+%     plot(error)
+end
+
