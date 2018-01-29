d5fed81016e391980790f3fe142f8610095b16b3
Elisabeth D <elisabeth.degrand@hotmail.fr>
2018-01-29 14:57:09 +0100

    main for the 3.1 part

--- plot1.m
+++ plot1.m
@@ -1,10 +1,9 @@
 function [] = plot1( data, W)
 %UNTITLED Plot the separation curve for a 1 layer perceptron
 %   Detailed explanation goes here
-
-gscatter(data(1,:),data(2,:),data(3,:))
+gscatter(data(1,:),data(2,:),data(3,:))   %plot the data
 xplot=-5:5;
 yplot=-W(1)/W(2).*xplot-(W(3)/W(2));
-plot(xplot,yplot);
+plot(xplot,yplot);    %plot the learning curve
 end
 
